f935647e5cf697b2159aa64ade51b474d7f66f97
setha <setha.sakti@iconpln.co.id>
2018-09-27 13:22:18 +0700

    belajar menggunakan component scan

--- src/main/java/aplikasi/SpringDemoApplication.java
+++ src/main/java/aplikasi/SpringDemoApplication.java
@@ -1,10 +1,15 @@
-package com.setha.SpringDemo;
+package aplikasi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.setha.SpringDemo.InjeksiViaConstructor;
+import com.setha.SpringDemo.InjeksiViaSetter;
 
 @SpringBootApplication
+@ComponentScan("com.setha.SpringDemo")
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
